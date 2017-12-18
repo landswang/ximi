fff15e12733908f1b750caf453b159140cc74680
torreswang <torreswangzh@gmail.com>
2017-12-18 10:58:29 +0800

    更改中文乱码问题

--- ximi_su/src/main/java/com/ejie/nbrs/util/TdesUtil.java
+++ ximi_su/src/main/java/com/ejie/nbrs/util/TdesUtil.java
@@ -1,9 +1,9 @@
 /**
  * DesUtil.java com.ejie.nbrs.test
  * 
- * Function锛� TODO
+ * Function： TODO
  * 
- * ver date author 鈹�鈹�鈹�鈹�鈹�鈹�鈹�鈹�鈹�鈹�鈹�鈹�鈹�鈹�鈹�鈹�鈹�鈹�鈹�鈹�鈹�鈹�鈹�鈹�鈹�鈹�鈹�鈹�鈹�鈹�鈹�鈹�鈹�鈹� ver1.1 2017骞�7鏈�17鏃� torreswang
+ * ver date author ────────────────────────────────── ver1.1 2017年7月17日 torreswang
  * 
  * Copyright (c) 2017, EJie All Rights Reserved.
  */
@@ -11,12 +11,12 @@
 package com.ejie.nbrs.util;
 
 /**
- * ClassName:des 鍔犺В瀵嗗伐鍏风被
+ * ClassName:des 加解密工具类
  * 
  * @author torreswang
  * @version
  * @since Ver 1.1
- * @Date 2017骞�7鏈�17鏃� 涓嬪崍2:13:50
+ * @Date 2017年7月17日 下午2:13:50
  * @see
  */
 import java.io.IOException;
@@ -36,18 +36,17 @@ public class TdesUtil {
   private final static String TKEY = "!Q@W3e4rqwe321";
 
   public static void main(String[] args) throws Exception {
-    String data = "98EMHytNpbc=";
+    String data = "DNbL3jJCPjHA7NlgvWrfDA==";
     String key = "!Q@W3e4rqwe321";
     System.err.println(encrypt(data, key));
     System.err.println(decrypt(data, key));
-    // System.err.println(encrypt("1234"));
   }
 
   /**
-   * Description 鏍规嵁閿�艰繘琛屽姞瀵�
+   * Description 根据键值进行加密
    * 
    * @param data
-   * @param key 鍔犲瘑閿産yte鏁扮粍
+   * @param key 加密键byte数组
    * @return
    * @throws Exception
    */
@@ -58,7 +57,7 @@ public class TdesUtil {
   }
 
   /**
-   * Description 鏍规嵁閿�艰繘琛屽姞瀵�
+   * Description 根据键值进行加密
    * 
    * @param data
    * @return
@@ -71,10 +70,10 @@ public class TdesUtil {
   }
 
   /**
-   * Description 鏍规嵁閿�艰繘琛岃В瀵�
+   * Description 根据键值进行解密
    * 
    * @param data
-   * @param key 鍔犲瘑閿産yte鏁扮粍
+   * @param key 加密键byte数组
    * @return
    * @throws IOException
    * @throws Exception
@@ -87,13 +86,13 @@ public class TdesUtil {
     byte[] buf = decoder.decodeBuffer(data);
     byte[] bt = decrypt(buf, TKEY.getBytes());
     return new String(bt);
-  }-
+  }
 
   /**
-   * Description 鏍规嵁閿�艰繘琛岃В瀵�
+   * Description 根据键值进行解密
    * 
    * @param data
-   * @param key 鍔犲瘑閿産yte鏁扮粍
+   * @param key 加密键byte数组
    * @return
    * @throws IOException
    * @throws Exception
@@ -109,28 +108,28 @@ public class TdesUtil {
   }
 
   /**
-   * Description 鏍规嵁閿�艰繘琛屽姞瀵�
+   * Description 根据键值进行加密
    * 
    * @param data
-   * @param key 鍔犲瘑閿産yte鏁扮粍
+   * @param key 加密键byte数组
    * @return
    * @throws Exception
    */
   private static byte[] encrypt(byte[] data, byte[] key) throws Exception {
-    // 鐢熸垚涓�涓彲淇′换鐨勯殢鏈烘暟婧�
+    // 生成一个可信任的随机数源
     SecureRandom sr = new SecureRandom();
 
-    // 浠庡師濮嬪瘑閽ユ暟鎹垱寤篋ESKeySpec瀵硅薄
+    // 从原始密钥数据创建DESKeySpec对象
     DESKeySpec dks = new DESKeySpec(key);
 
-    // 鍒涘缓涓�涓瘑閽ュ伐鍘傦紝鐒跺悗鐢ㄥ畠鎶奃ESKeySpec杞崲鎴怱ecretKey瀵硅薄
+    // 创建一个密钥工厂，然后用它把DESKeySpec转换成SecretKey对象
     SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(DES);
     SecretKey securekey = keyFactory.generateSecret(dks);
 
-    // Cipher瀵硅薄瀹為檯瀹屾垚鍔犲瘑鎿嶄綔
+    // Cipher对象实际完成加密操作
     Cipher cipher = Cipher.getInstance(DES);
 
-    // 鐢ㄥ瘑閽ュ垵濮嬪寲Cipher瀵硅薄
+    // 用密钥初始化Cipher对象
     cipher.init(Cipher.ENCRYPT_MODE, securekey, sr);
 
     return cipher.doFinal(data);
@@ -138,28 +137,28 @@ public class TdesUtil {
 
 
   /**
-   * Description 鏍规嵁閿�艰繘琛岃В瀵�
+   * Description 根据键值进行解密
    * 
    * @param data
-   * @param key 鍔犲瘑閿産yte鏁扮粍
+   * @param key 加密键byte数组
    * @return
    * @throws Exception
    */
   private static byte[] decrypt(byte[] data, byte[] key) throws Exception {
-    // 鐢熸垚涓�涓彲淇′换鐨勯殢鏈烘暟婧�
+    // 生成一个可信任的随机数源
     SecureRandom sr = new SecureRandom();
 
-    // 浠庡師濮嬪瘑閽ユ暟鎹垱寤篋ESKeySpec瀵硅薄
+    // 从原始密钥数据创建DESKeySpec对象
     DESKeySpec dks = new DESKeySpec(key);
 
-    // 鍒涘缓涓�涓瘑閽ュ伐鍘傦紝鐒跺悗鐢ㄥ畠鎶奃ESKeySpec杞崲鎴怱ecretKey瀵硅薄
+    // 创建一个密钥工厂，然后用它把DESKeySpec转换成SecretKey对象
     SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(DES);
     SecretKey securekey = keyFactory.generateSecret(dks);
 
-    // Cipher瀵硅薄瀹為檯瀹屾垚瑙ｅ瘑鎿嶄綔
+    // Cipher对象实际完成解密操作
     Cipher cipher = Cipher.getInstance(DES);
 
-    // 鐢ㄥ瘑閽ュ垵濮嬪寲Cipher瀵硅薄
+    // 用密钥初始化Cipher对象
     cipher.init(Cipher.DECRYPT_MODE, securekey, sr);
 
     return cipher.doFinal(data);
